129e177fb0cecd1c604b862ec91cc78d338d1093
Samarth2615 <164867341+Samarth2615@users.noreply.github.com>
2025-05-04 20:10:51 +0530

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -2,33 +2,21 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
-module.exports = defineConfig({
+export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
-module.exports = defineConfig({
-  projectConfig: {
-    // ...
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
-  },
-})
-module.exports = defineConfig({
-  // ...
-  admin: {
-    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-  },
-})
-module.exports = defineConfig({
-  projectConfig: {
-    // ...
     redisUrl: process.env.REDIS_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE as 'shared' | 'worker' | 'server',
+  },
+  admin: {
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
   },
 })
